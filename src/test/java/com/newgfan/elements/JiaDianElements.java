287e2584c6a40cb07ab32fcc0a9666aa7dc37e0a
ftangxiaohua@163.com <ftangxiaohua@163.com>
2016-11-17 16:55:52 +0800

    抽取页面结构相同的元素为公共元素

---
+++ src/test/java/com/newgfan/elements/JiaDianElements.java
@@ -0,0 +1,8 @@
+package com.newgfan.elements;
+
+/**
+ * Created by xiaohua on 11/17 0017.
+ */
+public class JiaDianElements {
+
+}
--- src/test/java/com/newgfan/elements/pubElements.java
+++ src/test/java/com/newgfan/elements/pubElements.java
@@ -15,11 +15,32 @@ public class pubElements {
         this.driver = driver;
     }
 
+    @FindBy(xpath = "/html/body/div[4]/div[1]/div[1]/div[1]/h2/a")
+    public WebElement firstTitle;
+    @FindBy(xpath = "/html/body/div[4]/div[1]/div[1]/div[2]/ul/li[1]/a[2]")
+    public WebElement secondTitle;
+    @FindBy(xpath = "html/body/div[5]/div[2]/div[2]/ul/li/a")
+    public List<WebElement> jifengjingxuan;
     @FindBy(xpath = "html/body/div[5]/div[2]/div[2]/div[2]/a")
     public WebElement aboutRead;
-
     @FindBy(xpath = "html/body/div[5]/div[2]/div[2]/ul/li[1]/a")
     public WebElement jingxuanTitle;
+    @FindBy(xpath = "/html/body/div[4]/div[2]/div/div[1]/a/img")
+    public WebElement tushangLeft;
+    @FindBy(xpath = "/html/body/div[4]/div[2]/div/div[2]/div/a")
+    public List<WebElement> tushangRight;
+    @FindBy(xpath = ".//*[@id='snav03-tab']/a")
+    public List<WebElement> tushangBelow_Nav;
+    @FindBy(xpath = ".//*[@id='snav03']/div[1]/div[2]/div[1]/div/div[1]/a[2]")
+    public WebElement zixunfirstpic;
+    @FindBy(xpath = ".//*[@id='enav03']/div[1]/div[2]/div[1]/div/div[1]/a[2]")
+    public WebElement jiadianzixunfirstpic;
+    @FindBy(xpath = ".//*[@id='iscroll-rightbox']/div[2]/a")
+    public WebElement jifengshidian_pic;
+    @FindBy(xpath = ".//*[@id='iscroll-rightbox']/div[1]/a")
+    public WebElement jifengshidian_more;
+    @FindBy(xpath = ".//*[@id='iscroll-rightbox']/ul/li/a")
+    public List<WebElement> wenzhangpaihang;
 
     //分类：标题前的分类
     @FindBy(xpath = "html/body/div[3]/div[1]/div[1]/div[1]/ul[1]/li[1]/a[1]")
@@ -33,6 +54,5 @@ public class pubElements {
     //点击标题前的分类进入的页面、点击【更多】后的机锋精选
     @FindBy(xpath = ".//*[@id='iscroll-rightbox']/div[2]/ul/li/a")
     public List<WebElement> classifyJingxuan;
-    @FindBy(xpath = ".//*[@id='iscroll-rightbox']/ul/li/a")
-    public List<WebElement> wenzhangpaihang;
+
 }
